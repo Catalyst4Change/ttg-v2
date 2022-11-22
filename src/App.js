60e4e0b1a44f89955d4db9e29ad7f240020d4418
Catalyst <103901063+Catalyst4Change@users.noreply.github.com>
2022-11-22 15:30:49 -0700

    complete form progression

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import React, { useState } from 'react'
+import { CardContainer } from './CardContainer/CardContainer';
 
 function App() {
+  const [playerCharacters, setPlayerCharacters] = useState([])
+
+  /*
+  {
+    "playerName": "Catalyst",
+    "charName": "Chefolopod",
+    "charConcept": "Food Bard Squid",
+    "charImage": "",
+    "attributes": {
+        "brawn": 3,
+        "agility": 3,
+        "intelligence": 1,
+        "wit": 2,
+        "charm": 2,
+        "presence": 2
+    },
+    "chosenMasteries": [
+        "Biology",
+        "Engineering"
+    ],
+    "chosenProficiencies": [
+        "Melee",
+        "Survival"
+    ]
+}
+  */
+
+
+  const [numberOfCards, setNumberOfCards] = useState(0)
+
+  const addPlayerCharacter = (newCharacter) => {
+    setPlayerCharacters([...playerCharacters, newCharacter])
+  }
+
+  const createNewCard = () => {      
+    console.log("card");
+    setNumberOfCards(numberOfCards => numberOfCards + 1)
+  }
+
+  const displayCards = () => {
+    for (let index = 0; index < numberOfCards; index++) {
+      console.log("numberOfCards", numberOfCards);
+      return (
+        <CardContainer addPlayerCharacter={addPlayerCharacter} playerCharacter={playerCharacters[index]} />
+      )
+    }
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={createNewCard} type='button'>+</button>
+      {displayCards()}
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/CharSheet.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+
+
+export const CharSheet = ({ playerCharacter }) => {
+
+  
+
+  return (
+    <div className='charsheet'>
+      CharSheet
+    </div>
+  )
+}
