a6de96408005a232f1be9dc506f89ac37bcf872a
Catalyst <103901063+Catalyst4Change@users.noreply.github.com>
2023-03-05 16:37:22 -0700

    complete health bar refactor

--- src/PlayerCardsContainer/PlayerCharacterCard/CharHealth/GenerateHealthBar.js
+++ src/PlayerCardsContainer/PlayerCharacterCard/CharHealth/GenerateHealthBar.js
@@ -1,4 +1,6 @@
 import greenDot from "../../../Assets/Images/icons8-green-circle-96.png";
+import yellowDot from "../../../Assets/Images/icons8-yellow-circle-96.png";
+import redDot from "../../../Assets/Images/icons8-red-circle-96.png";
 
 export const GenerateHealthBar = (maxHealth) => {
   let red = [];
@@ -6,8 +8,8 @@ export const GenerateHealthBar = (maxHealth) => {
   let green = [];
   let bar = [];
   for (let i = 0; i < maxHealth / 3; i++) {
-    red.push(<img className="health-bar-dot" src={greenDot} />);
-    yellow.push(<img className="health-bar-dot" src={greenDot} />);
+    red.push(<img className="health-bar-dot" src={redDot} />);
+    yellow.push(<img className="health-bar-dot" src={yellowDot} />);
     green.push(<img className="health-bar-dot" src={greenDot} />);
   }
   bar = red.concat(yellow, green);
