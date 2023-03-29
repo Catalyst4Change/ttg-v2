4eeeaf8061be489fd26f4112e7b7028ef71ac893
Catalyst <103901063+Catalyst4Change@users.noreply.github.com>
2023-03-29 07:48:06 -0600

    fix health bar generation

--- src/PlayerCardsContainer/PlayerCharacterCard/CharHealth/CharHealth.js
+++ src/PlayerCardsContainer/PlayerCharacterCard/CharHealth/CharHealth.js
@@ -2,71 +2,135 @@ import React, { useState, useEffect } from "react";
 import "./CharHealth.scss";
 import chevDown from "../../../Assets/Images/icons8-chevron-down-white.png";
 import chevUp from "../../../Assets/Images/icons8-chevron-up-white.png";
-import greenDot from "../../../Assets/Images/icons8-green-circle-96.png";
-import yellowDot from "../../../Assets/Images/icons8-yellow-circle-96.png";
-import redDot from "../../../Assets/Images/icons8-red-circle-96.png";
+import greenDotImage from "../../../Assets/Images/icons8-green-circle-96.png";
+import yellowDotImage from "../../../Assets/Images/icons8-yellow-circle-96.png";
+import redDotImage from "../../../Assets/Images/icons8-red-circle-96.png";
 
 export const CharHealth = ({
   maxHealth,
   currentHealth,
-  subtractHealth,
-  addHealth,
   healthRatio,
+  playerIndex,
+  playerCharacters,
+  setPlayerCharacters,
 }) => {
   const [condition, setCondition] = useState("Healthy (+1)");
+  const [healthBar, setHealthBar] = useState([]);
 
-  const generateHealthBar = () => {
-    console.log("maxHealth", maxHealth);
+  useEffect(() => {
     let red = [];
     let yellow = [];
     let green = [];
     let bar = [];
-    const translation = 21;
     const healthDots = maxHealth / 3;
     for (let i = 0; i < healthDots; i++) {
       red.push(
-        <circle
+        <img
           key={Math.random()}
-          transform={`translate(${i * translation}, ${11})`}
-          r="10.5"
-          strokeWidth={1}
-          stroke="darkred"
-          fill="red"
-        ></circle>
+          className="health-bar-dot"
+          src={redDotImage}
+          alt="red dot"
+        />
       );
       yellow.push(
-        <circle
+        <img
           key={Math.random()}
-          transform={`translate(${
-            translation * healthDots + i * translation
-          }, ${11})`}
-          r="10.5"
-          strokeWidth={1}
-          stroke="gold"
-          fill="yellow"
-        ></circle>
+          className="health-bar-dot"
+          src={yellowDotImage}
+          alt="yellow dot"
+        />
       );
       green.push(
-        <circle
+        <img
           key={Math.random()}
-          transform={`translate(${
-            translation * healthDots * 2 + i * translation
-          }, ${11})`}
-          r="10.5"
-          strokeWidth={1}
-          stroke="green"
-          fill="limegreen"
-        ></circle>
+          className="health-bar-dot"
+          src={greenDotImage}
+          alt="green dot"
+        />
       );
     }
+
     bar = red.concat(yellow, green);
-    console.log(bar);
-    return bar;
+    setHealthBar(bar);
+  }, []);
+
+  const addHealth = () => {
+    if (currentHealth < maxHealth) {
+      const healthDot = () => {
+        if (healthRatio < 0.33) {
+          return (
+            <img
+              key={Math.random()}
+              className="health-bar-dot"
+              src={redDotImage}
+              alt="red dot"
+            />
+          );
+        } else if (healthRatio >= 0.33 && healthRatio <= 0.66) {
+          return (
+            <img
+              key={Math.random()}
+              className="health-bar-dot"
+              src={yellowDotImage}
+              alt="yellow dot"
+            />
+          );
+        } else if (healthRatio >= 0.67) {
+          return (
+            <img
+              key={Math.random()}
+              className="health-bar-dot"
+              src={greenDotImage}
+              alt="green dot"
+            />
+          );
+        }
+      };
+
+      let healthBarCopy = healthBar;
+      healthBarCopy.push(healthDot());
+      setHealthBar(healthBarCopy);
+
+      if (currentHealth >= 1) {
+        setPlayerCharacters(
+          playerCharacters.map((character, i) => {
+            if (i === playerIndex) {
+              const updatedCharacter = {
+                ...character,
+                currentHealth: (currentHealth += 1),
+              };
+              return updatedCharacter;
+            }
+            return character;
+          })
+        );
+      }
+    }
+  };
+
+  const subtractHealth = () => {
+    const lessOne = healthBar.slice(0, -1);
+    setHealthBar(lessOne);
+
+    if (currentHealth >= 1) {
+      setPlayerCharacters(
+        playerCharacters.map((character, i) => {
+          if (i === playerIndex) {
+            const updatedCharacter = {
+              ...character,
+              currentHealth: currentHealth - 1,
+            };
+            return updatedCharacter;
+          }
+          return character;
+        })
+      );
+    }
   };
 
   const displayCondition = () => {
     if (healthRatio == 0.0) {
-      setCondition("DEAD");
+      setCondition("DEAD!");
     } else if (healthRatio > 0.0 && healthRatio <= 0.33) {
       setCondition("Unconscious");
     } else if (healthRatio >= 0.34 && healthRatio <= 0.67) {
@@ -87,7 +151,7 @@ export const CharHealth = ({
         <img
           key={Math.random()}
           className="health-bar-background-dot"
-          src={greenDot}
+          src={greenDotImage}
         />
       );
       dots.push(dot);
@@ -105,13 +169,13 @@ export const CharHealth = ({
       <div id="health-display">
         <div id="health-total">
           <div id="max-health">{maxHealth}</div>
-          <img className="health-total-background" src={greenDot} alt="dot" />
+          <img
+            className="health-total-background"
+            src={greenDotImage}
+            alt="dot"
+          />
         </div>
-        <svg height="22px" width="250px" className="health-bar">
-          <g transform={`translate(${centerX}, ${centerY})`}>
-            {generateHealthBar()}
-          </g>
-        </svg>
+        <div className="health-bar">{healthBar}</div>
         <div className="health-bar-background">{healthBarBackground()}</div>
       </div>
 
